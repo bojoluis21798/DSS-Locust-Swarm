41c2a96f9d4f5a159a9e9939654e8a2dd646b435
bojoluis21798 <trinity21798@gmail.com>
2018-06-29 13:51:12 +0800

    Place all other modules in src folder

---
+++ src/camera.py
@@ -0,0 +1,25 @@
+import cv2
+
+count = 0
+
+def __saveFrame(frame):
+	global count
+	cv2.imwrite('./img/'+str(count)+'.png', frame)
+	count+= 1
+
+def startCam():
+	cap = cv2.VideoCapture(0)
+
+	while(True):
+		ret, frame = cap.read()
+
+		cv2.imshow('DSS', frame)
+
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+		elif cv2.waitKey(1) & 0xFF == ord('e'):
+			__saveFrame(frame)
+
+	# When everything done, release the capture
+	cap.release()
+	cv2.destroyAllWindows()

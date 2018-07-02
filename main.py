b553866337db725859e2178e92f0a4df14cce8d8
bojoluis21798 <trinity21798@gmail.com>
2018-07-02 13:45:12 +0800

    Call detect method

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from src.camera import startCam
+from src.objectDetect import detect
 
 def main():
+    # Start camera.
     startCam()
+    # Draw Rectangle around object in the image.
+    detect()
 
 if __name__ == "__main__":
 	main()

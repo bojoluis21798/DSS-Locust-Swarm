7c33bf2bbca6b16f4a0cef7325a9679deb0b608d
bojoluis21798 <trinity21798@gmail.com>
2018-07-13 14:07:16 +0800

    Allow skip cam

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from src.camera import startCam
 from src.objectDetect import detect
+import sys
 
 def main():
-    # Start camera.
-    startCam()
-    # Draw Rectangle around object in the image.
-    detect()
+    if(len(sys.argv) == 1):
+        # Start camera.
+        startCam()
+        # Draw Rectangle around object in the image.
+        detect()
+    elif(sys.argv[1] == "skipcam"):
+        # proceed to detection.
+        detect()
+    else:
+        print "No such command"
 
 if __name__ == "__main__":
 	main()

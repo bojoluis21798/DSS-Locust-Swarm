d0545aa46b8d339410893d0d363c6b6e84c996e8
Carlo Janea <janeacarlo@gmail.com>
2018-07-20 23:51:55 +0800

    added scale identification but still very bad at accuracy

--- src/objectDetect.py
+++ src/objectDetect.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.signal import savgol_filter
+from scipy.spatial import distance as dist
 from scipy.interpolate import splprep, splev
 from imutils import contours as cnts
 from imutils import perspective
@@ -214,7 +215,62 @@ def orderingContours(img, contours):
 def sobelApproach(img):
     image_with_contour, contours = contoursWithSobel(img)
     smoothened_contour_img, smooth_contours = smoothingPoly(image_with_contour, contours, img)
-    return orderingContours(img, smooth_contours)
+    return getScale(img, smooth_contours)
+
+def midpoint(ptA, ptB):
+    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
+#same with orderingContours() but now measures the contours
+def getScale(img, contours):
+    (contours,_) = cnts.sort_contours(contours)
+    pixelsPerInch = None
+
+    for c in contours:
+        box = cv.minAreaRect(c)
+        box = cv.boxPoints(box)
+        box = np.array(box,dtype = "int")
+        
+        box = perspective.order_points(box)
+        cv.drawContours(img,[box.astype("int")], -1, (0,255,0), 2)
+
+        for (x, y) in box:
+		    cv.circle(img, (int(x), int(y)), 5, (0,0,255), -1)
+        
+        (tl,tr,bl,br) = box
+        (tltrX , tltrY) = midpoint(tl,tr)
+        (blbrX, blbrY) = midpoint(bl,br)
+
+        (tlblX,tlblY) = midpoint(tl,bl)
+        (trbrX, trbrY) = midpoint(tr,br)
+
+        cv.circle(img, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+        cv.circle(img, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+        cv.circle(img, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+        cv.circle(img, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+
+        # draw lines between the midpoints
+        cv.line(img, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)), (255, 0, 255), 2)
+        cv.line(img, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)), (255, 0, 255), 2)
+
+        # compute the Euclidean distance between the midpoints
+        dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+        dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+
+        if(pixelsPerInch is None):
+            pixelsPerInch = dA / 12.0
+        
+        # compute the size of the object
+        dimA = dA / pixelsPerInch
+        dimB = dB / pixelsPerInch
+
+        # draw the object sizes on the image
+        cv.putText(img, "{:.1f}in".format(dimA), (int(tltrX - 15), int(tltrY - 10)), 
+            cv.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+        cv.putText(img, "{:.1f}in".format(dimB),
+            (int(trbrX + 10), int(trbrY)), cv.FONT_HERSHEY_SIMPLEX,
+            0.65, (255, 255, 255), 2)
+    
+    return img
 
 def detectAll():
     i = 0
